132073c51a4bee0df43cba751096200a558b6320
tanguy ferric <tanguy.ferric@groupe-esigelec.org>
2019-10-17 20:11:10 +0000

    sf

---
+++ webapp/controller/data.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	
+], function() {
+	"use strict";
+
+	return ["sb-e7ddcbd1-6890-40ca-9062-c62f6da717ac!b25601|na-420adfc9-f96e-4090-a650-0386988b67e0!b1836", "9YVL0QhWky7o3ov6K4N9aROJeVM="]
+});
