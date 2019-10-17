fd1ae40cdd419750f95336e39308b74fcfbba618
gael29 <gael.lann@live.fr>
2019-10-17 20:04:59 +0000

    test

--- webapp/controller/data.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-	
-], function() {
-	"use strict";
-
-	return ["sb-9e179704-eef6-42cf-b6d1-ffbe6adfa04e!b25575|na-420adfc9-f96e-4090-a650-0386988b67e0!b1836", "DZzAPbAGnztzbaLYRozfenHhohI="]
-});
