3a7e644ad6aa18547ede2c53bcef0d53c181edc0
Nicolas CHABAL <nchabal44@gmail.com>
2019-10-17 20:10:42 +0000

    nioc le plus stylé du monde

--- webapp/controller/data.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	
-], function() {
-	"use strict";
-
-	return ["sb-ed67346f-99de-4625-8dc8-312b3d1739ac!b25564|na-420adfc9-f96e-4090-a650-0386988b67e0!b1836", "kgKBsjeofte4hHjd8Zb09vBnKQs="]
-});
-
-z
